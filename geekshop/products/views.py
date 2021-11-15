5b10cc04b6c9249dc5aeb1e703901c720d412742
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-11-15 16:15:57 +0300

    load dynamic data from json file

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from django.shortcuts import render
 
 
@@ -8,21 +11,8 @@ def main(request):
 
 
 def products(request):
-    content = {"title": "geekshop - Каталог",
-               "products":
-                   [
-                       {"img": "Adidas-hoodie.png", "title": "Худи черного цвета с монограммами adidas Originals",
-                        "price": "6 090,00", "text": "Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни"},
-                       {"img": "Blue-jacket-The-North-Face.png", "title": "Синяя куртка The North Face",
-                        "price": "23 725,00", "text": "Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель."},
-                       {"img": "Brown-sports-oversized-top-ASOS-DESIGN.png", "title": "Коричневый спортивный oversized-топ ASOS DESIGN",
-                        "price": "3 390,00", "text": "Материал с плюшевой текстурой. Удобный и мягкий."},
-                       {"img": "Black-Nike-Heritage-backpack.png", "title": "Черный рюкзак Nike Heritage",
-                        "price": "2 340,00", "text": "Плотная ткань. Легкий материал."},
-                       {"img": "Black-Dr-Martens-shoes.png", "title": "Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex",
-                        "price": "13 590,00", "text": "Гладкий кожаный верх. Натуральный материал."},
-                       {"img": "Dark-blue-wide-leg-ASOs-DESIGN-trousers.png", "title": "Темно-синие широкие строгие брюки ASOS DESIGN",
-                        "price": "2 890,00", "text": "Легкая эластичная ткань сирсакер Фактурная ткань."},
-                   ]
-               }
+    with open('products' + os.sep + 'fixtures' + os.sep + 'products-data.json', 'r',
+              encoding="utf-8") as json_file:
+        content = json.load(json_file)
+        content['title'] = 'geekshop - Каталог'
     return render(request, 'products/products.html', content)
