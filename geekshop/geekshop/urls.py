2f62bff691b073cb60601c0673cb72a4dd7c96bc
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-11-19 11:13:24 +0300

    Take data from DB

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import products.views as products
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', products.main, name='main'),
     path('products/', products.products, name='products'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
