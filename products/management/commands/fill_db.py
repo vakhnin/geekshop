8d93cc0ed82d13578faefc403ed66b7b6de76e71
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-11-22 15:09:21 +0300

    Add command fill_db

---
+++ products/management/commands/fill_db.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+import json, os
+
+from products.models import ProductCategory, Product
+
+JSON_PATH = 'products' + os.sep + 'fixtures'
+
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r', encoding='utf-8') as infile:
+        return json.load(infile)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        categories = load_from_json('categories')
+
+        ProductCategory.objects.all().delete()
+        for category in categories:
+            cat = category.get('fields')
+            cat['id'] = category.get('pk')
+            new_category = ProductCategory(**cat)
+            new_category.save()
+
+        products = load_from_json('products')
+
+        Product.objects.all().delete()
+        for product in products:
+            prod = product.get('fields')
+            category = prod.get('category')
+            _category = ProductCategory.objects.get(id=category)
+            prod['category'] = _category
+            new_category = Product(**prod)
+            new_category.save()
+
+        # Создаем суперпользователя при помощи менеджера модели
+        if not User.objects.filter(username='geekbrains'):
+            super_user = User.objects.create_superuser('geekbrains', 'geekbrains@geekshop.local', 'geekbrains')
+
