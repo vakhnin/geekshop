b9c0b0471d65932ae5ea0a5d5aae202805dc0062
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-11-23 10:10:14 +0300

    Make regisry user

--- authapp/models.py
+++ authapp/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
-    age = models.PositiveIntegerField(verbose_name='возраст')
+    age = models.PositiveIntegerField(verbose_name='возраст', default=18)
