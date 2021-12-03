f0bff6bb039f148d278cd2653f2a5d1bff13f752
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-12-03 09:52:22 +0300

    Add custom admin panel

---
+++ admins/urls.py
@@ -0,0 +1,12 @@
+from admins.views import index, admin_users_create, admin_users_update, admin_users_delete, admin_users
+from django.urls import path
+
+app_name = 'admins'
+urlpatterns = [
+
+    path('', index, name='index'),
+    path('users/', admin_users, name='admin_users'),
+    path('users-create/', admin_users_create, name='admin_users_create'),
+    path('users-update/<int:pk>', admin_users_update, name='admin_users_update'),
+    path('users-delete/<int:pk>', admin_users_delete, name='admin_users_delete'),
+]
