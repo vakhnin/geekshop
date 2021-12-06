96b55fc22ce7a0e50afa2ab376530666dad45ec5
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-12-06 09:18:12 +0300

    Add pagination on show products

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,8 @@ import products.views as products
 app_name = 'products'
 
 urlpatterns = [
-   path('', products.products, name='index'),
-   path('category/<int:pk>/', products.products, name='category'),
-   path('product/<int:pk>/', products.product, name='product'),
+    path('', products.products, name='index'),
+    path('category/<int:pk>/', products.products, name='category'),
+    path('page/<int:page>', products.products, name='page'),
+    path('product/<int:pk>/', products.product, name='product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product, ProductCategory
@@ -11,17 +12,31 @@ def main(request):
     return render(request, 'products/index.html', content)
 
 
-def products(request):
+def products(request, id_category=None, page=1):
     title = 'geekshop - Каталог'
-    products = Product.objects.all()
-    categories = ProductCategory.objects.filter(is_active=True)
-    content = {
+
+    if id_category:
+        products = Product.objects.filter(category_id=id_category)
+    else:
+        products = Product.objects.all()
+
+    paginator = Paginator(products, per_page=3)
+
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    products = products_paginator
+    categories = ProductCategory.objects.all()
+    context = {
         'title': title,
         'products': products,
         'categories': categories,
     }
-
-    return render(request, 'products/products.html', content)
+    return render(request, 'products/products.html', context)
 
 
 def product(request, pk):
