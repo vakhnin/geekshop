901d506395dded7db2992c8eaa5fdb8eac73b8eb
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-12-09 10:53:38 +0300

    Modify delete position in basket to CBV

--- baskets/urls.py
+++ baskets/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from baskets.views import basket_add, basket_remove, basket_edit
+from baskets.views import basket_add, basket_edit, BasketDeleteView
 
 app_name = 'baskets'
 urlpatterns = [
 
     path('add/<int:id>/', basket_add, name='basket_add'),
-    path('remove/<int:basket_id>/', basket_remove, name='basket_remove'),
+    path('remove/<int:pk>/', BasketDeleteView.as_view(), name='basket_remove'),
     path('edit/<int:id_basket>/<int:quantity>/', basket_edit, name='basket_edit'),
 
 ]
