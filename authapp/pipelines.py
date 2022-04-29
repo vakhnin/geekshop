fe471aad9bf441e2790eb309571dc22d2d4f2e8b
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-04-29 13:22:17 +0300

    Add user avatar from VK API

--- authapp/pipelines.py
+++ authapp/pipelines.py
@@ -15,7 +15,8 @@ def save_user_profile(backend, user, response, *args, **kwargs):
 
     api_url = urlunparse(('http', 'api.vk.com', 'method/users.get', None,
                           urlencode(
-                              OrderedDict(fields=','.join(('bdate', 'sex', 'about')),
+                              OrderedDict(fields=','.join(('bdate', 'sex', 'about',
+                                                           'has_photo', 'photo_max')),
                                           access_token=response['access_token'],
                                           v=5.131)), None))
 
@@ -23,22 +24,26 @@ def save_user_profile(backend, user, response, *args, **kwargs):
     if resp.status_code != 200:
         return
     data = resp.json()['response'][0]
+
     data_sex = {
         1: UserProfile.FEMALE,
         2: UserProfile.MALE,
         0: None
     }
 
-    user.userprofile.gender = data_sex[data['sex']]
-    if data['about']:
-        user.userprofile.about = data['about']
-
     bdate = datetime.strptime(data['bdate'], '%d.%m.%Y').date()
     age = timezone.now().date().year - bdate.year
-
     if age < 18:
         user.delete()
         raise AuthForbidden('social_core.backends.vk.VKOAuth2')
+
+    user.userprofile.gender = data_sex[data['sex']]
+
+    if data['about']:
+        user.userprofile.about = data['about']
+
+    if data['has_photo']:
+        user.image = data['photo_max']
 
     user.age = age
     user.save()
