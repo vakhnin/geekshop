809fc6ad61b13889939c98b64c434b78e4fbaa4f
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-05-03 10:22:17 +0300

    Correction styles

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -42,7 +42,7 @@ class OrderCreate(CreateView, BaseClassContextMixin):
                     form.initial['product'] = basket_item[num].product
                     form.initial['quantity'] = basket_item[num].quantity
                     form.initial['price'] = basket_item[num].product.price
-                # basket_item.delete()
+                basket_item.delete()
             else:
                 formset = OrderFormSet()
 
