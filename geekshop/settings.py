294a1a0888f1e007698ff03954f975ee2be14908
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-06-09 10:00:51 +0300

    Hide SECRET_KEY into .env file

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -18,15 +18,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-10i#4amk$x2tto=+if#cdym!()93we1(bu%crd8*zt$8*msvj('
-
 from dotenv import load_dotenv
 
 load_dotenv(BASE_DIR / '.env')
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
