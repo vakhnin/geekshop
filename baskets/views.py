7228a0d99b772f04573e5d19805c5e13d482c9fc
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-06-10 10:49:31 +0300

    Delete commented code

--- baskets/views.py
+++ baskets/views.py
@@ -24,12 +24,8 @@ def basket_add(request, id):
         baskets = Basket.objects.filter(user=user_select, product=product)
         if baskets:
             basket = baskets.first()
-            # basket.quantity += 1
             basket.quantity = F('quantity') + 1
             basket.save()
-
-            # update_queries = list(filter(lambda x: 'UPDATE' in x['sql'], connection.queries))
-            # print(f'basket_add {update_queries} ')
         else:
             Basket.objects.create(user=user_select, product=product, quantity=1)
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
