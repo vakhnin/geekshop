efb1902ca44291b2c082d48e4137342e7a05bc4c
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2023-11-26 08:55:10 +0300

    Redirect not login user from admin category, product, order pages

--- admins/views/categories.py
+++ admins/views/categories.py
@@ -1,34 +1,67 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from admins.forms import ProductCategoryAdminForm
-from products.mixin import AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin
+from products.mixin import AddTitleAndNavActiveToContextMixin
 from products.models import ProductCategory
-from django.db import connection
 
 
-class CategoryCreateView(CreateView, AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin):
+class CategoryCreateView(CreateView, LoginRequiredMixin, AddTitleAndNavActiveToContextMixin):
     model = ProductCategory
     template_name = 'admins/admin-categories-create.html'
     success_url = reverse_lazy('admins:admin_categories')
     form_class = ProductCategoryAdminForm
     title = 'Админка | Создание категории'
 
+    login_url = '/auth/login-required'
+    redirect_field_name = 'redirect_to'
 
-class CategoryListView(ListView, AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin):
+    no_permission_template_name = 'admins/admin-no-permission.html'
+
+    def get_template_names(self):
+        if self.request.user.is_staff:
+            return super().get_template_names()
+        else:
+            return self.no_permission_template_name
+
+
+class CategoryListView(ListView, LoginRequiredMixin, AddTitleAndNavActiveToContextMixin):
     model = ProductCategory
     template_name = 'admins/admin-categories-read.html'
     title = 'Админка | Список категорий'
 
+    login_url = '/auth/login-required'
+    redirect_field_name = 'redirect_to'
 
-class CategoryUpdateView(UpdateView, AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin):
+    no_permission_template_name = 'admins/admin-no-permission.html'
+
+    def get_template_names(self):
+        if self.request.user.is_staff:
+            return super().get_template_names()
+        else:
+            return self.no_permission_template_name
+
+
+class CategoryUpdateView(UpdateView, LoginRequiredMixin, AddTitleAndNavActiveToContextMixin):
     model = ProductCategory
     template_name = 'admins/admin-categories-update-delete.html'
     form_class = ProductCategoryAdminForm
     title = 'Админка | Обновление категории'
     success_url = reverse_lazy('admins:admin_categories')
+
+    login_url = '/auth/login-required'
+    redirect_field_name = 'redirect_to'
+
+    no_permission_template_name = 'admins/admin-no-permission.html'
+
+    def get_template_names(self):
+        if self.request.user.is_staff:
+            return super().get_template_names()
+        else:
+            return self.no_permission_template_name
 
     def form_valid(self, form):
         if 'discount' in form.cleaned_data:
@@ -39,9 +72,20 @@ class CategoryUpdateView(UpdateView, AddTitleAndNavActiveToContextMixin, UserIsS
         return super().form_valid(form)
 
 
-class CategoryDeleteView(DeleteView, AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin):
+class CategoryDeleteView(DeleteView, LoginRequiredMixin, AddTitleAndNavActiveToContextMixin):
     model = ProductCategory
     success_url = reverse_lazy('admins:admin_categories')
+
+    login_url = '/auth/login-required'
+    redirect_field_name = 'redirect_to'
+
+    no_permission_template_name = 'admins/admin-no-permission.html'
+
+    def get_template_names(self):
+        if self.request.user.is_staff:
+            return super().get_template_names()
+        else:
+            return self.no_permission_template_name
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
--- admins/views/products.py
+++ admins/views/products.py
@@ -1,38 +1,83 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from admins.forms import ProductAdminForm
-from products.mixin import AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin
+from products.mixin import AddTitleAndNavActiveToContextMixin
 from products.models import Product
 
 
 # Create your views here.
-class ProductCreateView(CreateView, AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin):
+class ProductCreateView(CreateView, LoginRequiredMixin, AddTitleAndNavActiveToContextMixin):
     model = Product
     template_name = 'admins/admin-products-create.html'
     success_url = reverse_lazy('admins:admin_products')
     form_class = ProductAdminForm
     title = 'Админка | Создание продукта'
 
+    login_url = '/auth/login-required'
+    redirect_field_name = 'redirect_to'
 
-class ProductListView(ListView, AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin):
+    no_permission_template_name = 'admins/admin-no-permission.html'
+
+    def get_template_names(self):
+        if self.request.user.is_staff:
+            return super().get_template_names()
+        else:
+            return self.no_permission_template_name
+
+
+class ProductListView(ListView, LoginRequiredMixin, AddTitleAndNavActiveToContextMixin):
     model = Product
     template_name = 'admins/admin-products-read.html'
     title = 'Админка | Список продуктов'
 
+    login_url = '/auth/login-required'
+    redirect_field_name = 'redirect_to'
 
-class ProductUpdateView(UpdateView, AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin):
+    no_permission_template_name = 'admins/admin-no-permission.html'
+
+    def get_template_names(self):
+        if self.request.user.is_staff:
+            return super().get_template_names()
+        else:
+            return self.no_permission_template_name
+
+
+class ProductUpdateView(UpdateView, LoginRequiredMixin, AddTitleAndNavActiveToContextMixin):
     model = Product
     template_name = 'admins/admin-products-update-delete.html'
     form_class = ProductAdminForm
     title = 'Админка | Обновление продукта'
     success_url = reverse_lazy('admins:admin_products')
 
+    login_url = '/auth/login-required'
+    redirect_field_name = 'redirect_to'
 
-class ProductDeleteView(DeleteView, AddTitleAndNavActiveToContextMixin, UserIsSuperuserMixin):
+    no_permission_template_name = 'admins/admin-no-permission.html'
+
+    def get_template_names(self):
+        if self.request.user.is_staff:
+            return super().get_template_names()
+        else:
+            return self.no_permission_template_name
+
+
+class ProductDeleteView(DeleteView, LoginRequiredMixin, AddTitleAndNavActiveToContextMixin):
     model = Product
     success_url = reverse_lazy('admins:admin_products')
+
+    login_url = '/auth/login-required'
+    redirect_field_name = 'redirect_to'
+
+    no_permission_template_name = 'admins/admin-no-permission.html'
+
+    def get_template_names(self):
+        if self.request.user.is_staff:
+            return super().get_template_names()
+        else:
+            return self.no_permission_template_name
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
